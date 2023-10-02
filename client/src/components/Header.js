5c86961b2f641f43b5e871c491aef8bf454f411b
ammarrqureshi <mamqureshi786@gmail.com>
2023-10-02 12:34:11 +0500

    header modified

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,52 @@
-import React from "react";
+import { useState } from "react";
 import logo from "../assets/writesmith logo.svg";
 import { Link } from "react-router-dom";
 const Header = () => {
-  const showalert = () =>{
-    alert("Wait!");
+  const login= () =>{
+    alert("Logging In!");
+    setLoggedIn(true);
   }  
+
+  const logout = () =>{
+    setLoggedIn(false);
+  }
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  let headerButton;
+  if (loggedIn) {
+          
+    console.log("loggedin")
+    headerButton=
+  
+    <button onClick={logout}>
+      Logout
+    </button>
+   
+  }
+
+  else{
+    headerButton=
+   <>
+  
+    <button onClick={login}>
+      Login
+    </button>
+    
+    <Link to="/signup">
+    <button >
+      Sign Up
+    </button>
+    </Link></>
+  }
   return (
     <>
       <header className="App-header">
-      <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>  
+      <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link> 
+
       <div className="header-right">
-      <Link to="/login">
-      <button onClick={showalert}>
-        Login
-      </button>
-      </Link>
-      <Link to="/signup">
-      <button onClick={showalert}>
-        Sign Up
-      </button>
-      </Link>
+        
+    
+     {headerButton}
       </div>
      
       </header>
