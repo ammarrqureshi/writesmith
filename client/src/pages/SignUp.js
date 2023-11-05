ec184cf99587af82cf7bcffe6fd1b5a45410d996
ammarrqureshi <mamqureshi786@gmail.com>
2023-11-05 12:19:48 +0500

    backend connected to signup page

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SignUp = () => {
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+ async function signUp(e) {
+    e.preventDefault();
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      body: JSON.stringify({fullname,email,password}),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
   return (
-    <div className='signup'>
+    <div className="signup">
+      <form action="" onSubmit={signUp}>
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={fullname}
+          onChange={(e) => setFullName(e.target.value)}
+        />
 
-    <form action=''>
-    <input type="text"  placeholder='Full Name'/>
-
-        <input type="email"  placeholder='Email'/>
-        <input type="password"  placeholder='Password'/>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button> Sign Up</button>
-    </form>
-</div>
-  )
-}
+      </form>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
