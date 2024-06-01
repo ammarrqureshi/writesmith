e8e6e7ea4914692895ab8e137e38db0054f374cf
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-02 01:26:54 +0500

    api dir

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <>
+      <div>
+        <DatePicker />
+        Hello Writesmith
+      </div>
+    </>
+  );
+}
+
+export default App;
