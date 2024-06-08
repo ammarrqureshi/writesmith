9731209ac5d8b10efe8ae8ad18f1b9ebe3046937
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-08 20:28:37 +0500

    basic routes set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Navbar } from "./components/Navbar";
+import { PostPage } from "./pages/PostPage";
 
 const Layout = () => {
   return (
@@ -21,10 +22,19 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <h1>Oops!  where have you come you stupid! </h1> ,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/p/:id",
+        element: <PostPage />,
+      },
+      {
+        path: "/write",
+        element: <h1>write here...</h1>,
       },
     ],
   },
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Button } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
+import { PostPage } from "./PostPage";
 
 export const Home = () => {
   return (
     <div>
+      <PostPage/>
       <Link to="/login">
         <Button type="primary">Login</Button>
       </Link>
