309b1578179f22cb11254f3b6bf3f486cf900c2f
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-08 20:43:59 +0500

    login form added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#fa8c16",
+          colorInfo: "#fa8c16",
+          colorSuccess: "#00a600",
+          borderRadius: 10,
+          wireframe: false,
+        },
+        components: {
+          Button: {
+            controlHeight: 40,
+            controlHeightLG: 46,
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,53 @@
- 
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import React from "react";
+import type { FormProps } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+export const Login = () => (
+  <Form
+    name="basic"
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: "Please input your username!" }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: "Please input your password!" }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item<FieldType> name="remember" valuePropName="checked">
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
