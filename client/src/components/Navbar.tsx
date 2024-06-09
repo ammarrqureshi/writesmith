dffbde6a1a8286290bde52c173ec52dd41a02688
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-10 02:16:09 +0500

    navbar style

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,14 @@
+import { Button } from "antd";
+import { Text } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <Button type="text" icon={<Text />} />
+
+      <div>
+        <img src="./assets/writesmith-logo.svg" alt="" />
+      </div>
+    </div>
+  );
+};
