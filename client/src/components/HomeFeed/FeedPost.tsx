14e73e22daa5908f3fc9f2e46b523e342f72f51f
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-22 22:19:42 +0500

    suspense for feedpost

--- client/src/components/HomeFeed/FeedPost.tsx
+++ client/src/components/HomeFeed/FeedPost.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Button, Card } from "antd";
 import Meta from "antd/es/card/Meta";
+import { Suspense } from "react";
 import { FaRegComment, FaRegHeart, FaRegPaperPlane } from "react-icons/fa";
 import { PostType } from "src/types/types";
 
@@ -9,22 +10,24 @@ interface FeedPostProps {
 export const FeedPost = ({ data }: FeedPostProps) => {
   return (
     <div>
-      <Card className="w-96" cover={<img alt="example" src={data.img} />}>
-        <Meta
-          avatar={
-            <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
-          }
-          title={data.title}
-          description={data.desc}
-        />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Card className="w-96" cover={<img alt="example" src={data.img} />}>
+          <Meta
+            avatar={
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+            }
+            title={data.title}
+            description={data.desc}
+          />
 
-        <div className="flex justify-between w-full">
-          <Button type="link" icon={<FaRegHeart size={20} />} />
-          <Button type="link" icon={<FaRegComment size={20} />} />
+          <div className="flex justify-between w-full">
+            <Button type="link" icon={<FaRegHeart size={20} />} />
+            <Button type="link" icon={<FaRegComment size={20} />} />
 
-          <Button type="link" icon={<FaRegPaperPlane size={20} />} />
-        </div>
-      </Card>
+            <Button type="link" icon={<FaRegPaperPlane size={20} />} />
+          </div>
+        </Card>
+      </Suspense>
     </div>
   );
 };
