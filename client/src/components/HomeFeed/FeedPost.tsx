d63bb4b0a189f4af4403f316910456347ece6eae
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-23 12:12:50 +0500

    author link added

--- client/src/components/HomeFeed/FeedPost.tsx
+++ client/src/components/HomeFeed/FeedPost.tsx
@@ -4,7 +4,6 @@ import { FaRegComment, FaRegHeart, FaRegPaperPlane } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { PostType } from "src/types/types";
 
-
 interface FeedPostProps {
   data: PostType;
 }
@@ -15,9 +14,11 @@ export const FeedPost = ({ data }: FeedPostProps) => {
     <div>
       <Suspense fallback={<h1>Loading...</h1>}>
         <Card className="w-[40%]" cover={<img alt="example" src={data.img} />}>
+          <Text type="secondary">{data.timeToRead}</Text>
           <Link to={`/p/${data.id}`}>
-            <Text type="secondary">{data.timeToRead}</Text>
-            <Title level={2}>{data.title}</Title>
+            <Title level={2}>{data.title}</Title>{" "}
+          </Link>
+          <Link to={`/author/123`}>
             <div className="flex gap-4">
               <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
               <div className="flex flex-col">
@@ -32,7 +33,6 @@ export const FeedPost = ({ data }: FeedPostProps) => {
               </div>
             </div>
           </Link>
-
           <div className="flex justify-between w-full py-6">
             <Button type="link" icon={<FaRegHeart size={20} />} />
             <Button type="link" icon={<FaRegComment size={20} />} />
