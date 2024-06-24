cc9bf9341fa56b48dd5d7f0563febe1cbeebb76d
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-24 19:02:37 +0500

    toolbar fixed

--- client/src/pages/WritePost/Editor.tsx
+++ client/src/pages/WritePost/Editor.tsx
@@ -1,6 +1,7 @@
 import { Button, Card } from "antd";
 import { useState } from "react";
-import ReactQuill from "react-quill";
+import { AiOutlineAlert } from "react-icons/ai";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 export const Editor = () => {
@@ -10,15 +11,33 @@ export const Editor = () => {
     },
     theme: "snow",
 
-    placeholder: "Compose an epic...",
+    placeholder: "Start writing here...",
   };
   const [content, setContent] = useState("");
   console.log(content);
 
   return (
-    <div>
-      <Card size="small" padding={0}id="toolbar" className="">
-        <Button style={{ color: "red" }} className="ql-bold " type="text" />
+    <div className=" my-10 flex flex-col gap-8 relative">
+      <Card
+        size="small"
+        style={{ padding: 0}}
+        id="toolbar"
+        className="sticky top-5 bg-white z-50"
+      >
+        <div className="flex justify-around w-full">
+          <Button className="ql-bold " type="text"></Button>
+          <Button className="ql-italic" type="text"></Button>
+          <Button className="ql-underline " type="text"></Button>
+          <Button className="ql-strike " type="text"></Button>
+          <Button className="ql-link " type="text"></Button>
+          <Button className="ql-code-block " type="text"></Button>
+
+          <Button className="ql-list " value="ordered" type="text"></Button>
+          <Button className="ql-list " value="bullet" type="text"></Button>
+          <Button className="ql-image " type="text"></Button>
+          <Button className="ql-header " value="1" type="text"></Button>
+          <Button className="ql-header " value="2" type="text"></Button>
+        </div>
       </Card>
       <ReactQuill {...quill_options} value={content} onChange={setContent} />
     </div>
