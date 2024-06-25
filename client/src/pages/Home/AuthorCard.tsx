bf733a2509ff777eb2d3425ae02ae00602c239c3
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-25 23:42:47 +0500

    clickable author card

--- client/src/pages/Home/AuthorCard.tsx
+++ client/src/pages/Home/AuthorCard.tsx
@@ -1,22 +1,25 @@
 import { Avatar, Button, Card, Typography } from "antd";
 import { AiOutlineUserAdd } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import { AuthorType } from "src/types/types";
 
 const { Text } = Typography;
 
 export const AuthorCard = ({ authorData }: { authorData: AuthorType }) => {
   return (
-    <div>
-      <Card size="small">
-        <div className="flex gap-4 justify-around items-center">
-          <Avatar size={50} src={authorData.avatar} />
-          <div className="flex flex-col">
-            <Text strong>{authorData.name}</Text>
-            <Text>{authorData.designation}</Text>
+    <Link to="/author/123">
+      <div>
+        <Card size="small">
+          <div className="flex gap-4 justify-around items-center">
+            <Avatar size={50} src={authorData.avatar} />
+            <div className="flex flex-col">
+              <Text strong>{authorData.name}</Text>
+              <Text>{authorData.designation}</Text>
+            </div>
+            <Button type="text" icon={<AiOutlineUserAdd size={20} />} />
           </div>
-          <Button type="text" icon={<AiOutlineUserAdd size={20} />} />
-        </div>
-      </Card>
-    </div>
+        </Card>
+      </div>{" "}
+    </Link>
   );
 };
